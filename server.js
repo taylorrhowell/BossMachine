0397578dd02f109f5734f4fd3347956ab5e163bf
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-09-21 20:43:14 -0700

    finish server boilerplate

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 
 module.exports = app;
 
@@ -14,16 +14,16 @@ const PORT = process.env.PORT || 4001;
 app.use(cors());
 
 // Add middware for parsing request bodies here:
-app.use(bodyparser);
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-  appp.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
   });
 };
