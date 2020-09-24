14a12042ceb70a66e7be2a4ed3b1687d3174072e
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-09-23 21:26:25 -0700

    add and test first route w/ nested routers - pass

--- server/minions.js
+++ server/minions.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const { getAllFromDatabase } = require('./db');
+const minionsRouter = express.Router();
+
+minionsRouter.get('/', (req, res, next) => {
+    const minions = getAllFromDatabase('minions');
+    res.status(200).send(minions);
+});
 /* Routes Required
 
 /api/minions
@@ -9,3 +17,4 @@ PUT /api/minions/:minionId to update a single minion by id.
 DELETE /api/minions/:minionId to delete a single minion by id. 
 
 For all /api/minions and /api/ideas routes, any POST or PUT requests will send their new/updated resources in the request body. POST request bodies will not have an id property, you will have to set it based on the next id in sequence.*/
+module.exports = minionsRouter;
