0841c65e3495760dbf1364bf97048d4494e44ca4
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-09-25 20:28:36 -0700

    complete and hook up meetingsRouter to API

--- server/api.js
+++ server/api.js
@@ -4,10 +4,12 @@ const apiRouter = express.Router();
 //Require in all nested routes
 const minionsRouter = require('./minions');
 const ideasRouter = require('./ideas');
+const meetingsRouter = require('./meetings');
 
 //Route respective requests to routers 
 apiRouter.use('/minions', minionsRouter);
 apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
 //export API
 module.exports = apiRouter;
--- server/meetings.js
+++ server/meetings.js
@@ -1,8 +1,24 @@
-/* Required Routes
-/api/meetings
+const express = require('express');
+const { deleteAllFromDatabase, getAllFromDatabase, createMeeting, addToDatabase } = require('./db');
+const meetingsRouter = express.Router();
 
-GET /api/meetings to get an array of all meetings.
-POST /api/meetings to create a new meeting and save it to the database.
-DELETE /api/meetings to delete all meetings from the database. 
+//GET /api/meetings to get an array of all meetings.
+meetingsRouter.get('/', (req, res, next) => {
+    const meetings = getAllFromDatabase('meetings');
+    res.status(200).send(meetings);
+});
 
-For /api/meetings POST route, no request body is necessary, as meetings are generated automatically by the server upon request. Use the provided createMeeting function exported from db.js to create a new meeting object. */
+//POST /api/meetings to create a new meeting and save it to the database. For /api/meetings POST route, no request body is necessary, as meetings are generated automatically by the server upon request. Use the provided createMeeting function exported from db.js to create a new meeting object.
+meetingsRouter.post('/', (req, res, next) => {
+    const newMeeting = createMeeting();
+    addToDatabase('meetings', newMeeting);
+    res.status(201).send(newMeeting);
+});
+
+//DELETE /api/meetings to delete all meetings from the database. 
+meetingsRouter.delete('/', (req, res, next) => {
+    deleteAllFromDatabase('meetings');
+    res.status(204).send();
+});
+
+module.exports = meetingsRouter;
