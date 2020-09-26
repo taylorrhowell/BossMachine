23a62a25de56cdb82ec580d869b9b4b3729f9e9f
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-09-25 21:59:12 -0700

    complete minion work get route

--- server/minions.js
+++ server/minions.js
@@ -42,5 +42,42 @@ minionsRouter.delete('/:minionId', (req, res, next) => {
     deleteFromDatabasebyId('minions', req.minion.id);
     res.status(204).send();
 });
+/* BONUS: /api/minions/:minionId/work routes
+GET /api/minions/:minionId/work
+- returns an array
+- returns an array of all all work for the specified minion
+- called with a non-numeric minion ID returns a 404 error
+- called with an invalid ID minion returns a 404 error */
+minionsRouter.get('/:minionId/work', (req, res, next) => {
+    //Array of work objects for every minion
+    const allWork = getAllFromDatabase('work');
+    const minionWork = [];
+    allWork.forEach((work) => {
+        if (work.minionId === req.minion.id) {
+            minionWork.push(work);
+        };
+    });
+    res.status(200).send(minionWork);
+});
 
+/* PUT /api/minions/:minionId/work/:workId
+- updates the correct work and returns it
+- updates the correct work item and persists to the database
+- called with a non-numeric minion ID returns a 404 error
+- called with an invalid minion ID returns a 404 error
+- called with a non-numeric work ID returns a 404 error
+- called with an invalid work ID returns a 404 error
+- called with an invalid ID does not change the database array
+- returns a 400 if a work ID with the wrong :minionId is requested */
+
+
+/* POST /api/minions/:minionId/work
+- should add a new work item if all supplied information is correct */
+
+/* DELETE /api/minions/:minionId/work/:workId
+- deletes the correct work by id
+- called with a non-numeric minion ID returns a 404 error
+- called with an invalid minion ID returns a 404 error
+- called with a non-numeric work ID returns a 404 error
+- called with an invalid work ID returns a 404 error */
 module.exports = minionsRouter;
